efb1f765ee4230b1bebcdc8e5ebd20b010664e15
Will Handley <wh260@cam.ac.uk>
2018-10-05 10:53:02 +0100

    last part of test.py before removal

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ h = 0.6732
 Omega_b = 0.022383     / h**2
 Omega_m = 0.14314      / h**2
 Omega_r = 4.18343e-5   / h**2 # Assumes T = 2.7255, and includes neutrinos
-Omega_k_ = -0.1
+Omega_k_ = -0.01
 Omega_l = 1-Omega_m-Omega_r-Omega_k_
 H0 = H0_h * h
 logA = 3.0448
@@ -27,8 +27,8 @@ def trans(x, y, K, V):
     phi_e = phi_min + x+numpy.sqrt(1+x**2)
     Nmax = numpy.log(2*(kstar**2 + K)/V(phi_e))/2
     Nmin = -numpy.log(V(phi_e)/2)/2
-    N_e = Nmin + (Nmax-Nmin)*numpy.exp(y)/(1+numpy.exp(y))
-    print(N_e, phi_e)
+    N_e = Nmin + (Nmax-Nmin) * (0.5 + numpy.arctan(y)/numpy.pi) #*numpy.exp(y)/(1+numpy.exp(y))
+    print(x, y, N_e, phi_e)
     return N_e, phi_e
 
 
@@ -65,12 +65,28 @@ def f(x):
     sol = integrate_from_inflation_start(N_e, phi_e, K, V)
     logAs, ns, logOmega_k = compute_pps(sol, K, V)
 
-    return [ns - ns_, 2*logOmega_k - logAs - numpy.log(c)]
+    return [ns - ns_, 2*logOmega_k - 2*D - logAs - numpy.log(c)]
+
+D = 2
+
+
+K = 1
+m = 1
+V = ChaoticPotential(m)
+N_e = -1.9
+phi_e = 17
+sol = integrate_from_inflation_start(N_e, phi_e, K, V)
+plt.plot(sol.N-N_e, sol.dphi)
 
 
 
 from scipy.optimize import root
 sol = root(f, [0, 0])
+print(sol)
+f([4.35,5])
+
+import sys
+sys.exit(0)
 
 K = 1
 m = 1
@@ -90,3 +106,30 @@ print("-----------------")
 print(logAs - logAs_)
 print(ns - ns_)
 print(logOmega_k - numpy.log(-Omega_k_))
+
+plt.plot(sol.N,-(sol.N + numpy.log(sol.H)))
+0.05/numpy.exp(numpy.log(kstar)-(sol.N[0] + numpy.log(sol.H[0])))
+
+
+
+xs = numpy.linspace(4.2,5.2,40)
+ys = numpy.linspace(-15,20,40)
+fs = numpy.array([[f([x,y]) for x in xs] for y in ys])
+tran =  numpy.array([[trans(x,y, K, V) for x in xs] for y in ys]) 
+
+fig, ax = plt.subplots()
+color = ax.contourf(xs, ys, fs[:,:,0], [-1e-4,0,1e-4], colors='k')
+color = ax.contourf(xs, ys, fs[:,:,1]-4, [-1,0,1], colors='r')
+
+fig, ax = plt.subplots()
+color = ax.contour(xs, ys, tran[:,:,0])
+color = ax.contour(xs, ys, tran[:,:,1])
+
+for i in range(len(ys)):
+    plt.plot(fs[i,:,0], fs[i,:,1],'k-')
+
+
+for i in range(len(xs)):
+    plt.plot(fs[:,i,0], fs[:,i,1],'r-')
+
+
