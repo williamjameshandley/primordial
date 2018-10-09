ed7b23c0eccd33bf89a2421f9be6ca929bef7f38
Will Handley <wh260@cam.ac.uk>
2018-10-09 18:57:07 +0100

    Testing in all curved cases now

--- primordial/test/test_cosmology.py
+++ primordial/test/test_cosmology.py
@@ -13,28 +13,33 @@ def test_cosmology():
     Omega_b = 0.022383     / h**2
     Omega_m = 0.14314      / h**2
     Omega_r = 4.18343e-5   / h**2 # Assumes T = 2.7255, and includes neutrinos
-    Omega_k = 0#-0.01
-    Omega_l = 1-Omega_m-Omega_r-Omega_k
     H0 = H0_h * h
-    Ni = -70
 
-    # Solve in N
-    equations = Eqs_N(H0, Omega_r, Omega_m, Omega_k, Omega_l)
-    ic = IC_N(Ni)
-    events = UntilN(equations, terminal=True,value=10)
-    sol_N = solve(equations, ic, events=events, atol=atol, rtol=rtol)
+    for Omega_k in [-0.1, 0, 0.1]:
+        Omega_l = 1-Omega_m-Omega_r-Omega_k
 
-    N = sol_N.N[1:-1]
-    t = sol_N.t(N)
+        # Solve in N
+        equations = Eqs_N(H0, Omega_r, Omega_m, Omega_k, Omega_l)
 
-    # Solve in t
-    equations = Eqs_t(H0, Omega_r, Omega_m, Omega_k, Omega_l)
-    ic = IC_t(Ni)
-    events = UntilN(equations, terminal=True, value=10)
-    sol_t = solve(equations, ic, events=events, t_eval=t, atol=atol, rtol=rtol)
+        Ni = 70 if Omega_k else -70
+        ic = IC_N(Ni)
 
-    ## Check t-N consistency
-    atol *= 10
-    rtol *= 10
-    assert_allclose(sol_t.N(t), N, atol, rtol)
-    assert_allclose(t, sol_N.t(N), atol, rtol)
+        Ne = equations.N0 if Omega_k else 0
+        events = UntilN(equations, terminal=True, value=Ne)
+        sol_N = solve(equations, ic, events=events, atol=atol*1e-1, rtol=rtol*1e-1)
+
+        N = sol_N.N[1:-1]
+        t = sol_N.t(N)
+
+        # Solve in t
+        equations = Eqs_t(H0, Omega_r, Omega_m, Omega_k, Omega_l)
+        ic = IC_t(Ni)
+        events = UntilN(equations, terminal=True, value=Ne)
+        sol_t = solve(equations, ic, events=events, t_eval=t, atol=atol*1e-1, rtol=rtol*1e-1)
+
+        ## Check t-N consistency
+        assert_allclose(sol_t.N(t), N, atol, rtol)
+        assert_allclose(t, sol_N.t(N), atol, rtol)
+
+        # Check H
+        assert_allclose(sol_N.H(N), sol_t.H(t), atol*10, rtol*10)
--- primordial/test/test_inflation.py
+++ primordial/test/test_inflation.py
@@ -10,33 +10,30 @@ def test_inflation():
     atol, rtol = 1e-8, 1e-8
 
     V = ChaoticPotential(m=1)
-    K = 0
-
     N_0, phi_0 = 0, 17
 
-    # Solve in N
-    equations = Eqs_N(K, V)
-    ic = IC_N(N_0, phi_0)
-    events = Inflation(equations, direction=-1, terminal=True)
-    sol_N = solve(equations, ic, events=events, atol=atol, rtol=rtol)
+    for K in [-1, 0, 1]:
+        # Solve in N
+        equations = Eqs_N(K, V)
+        ic = IC_N(N_0, phi_0)
+        events = Inflation(equations, direction=-1, terminal=True)
+        sol_N = solve(equations, ic, events=events, atol=atol*1e-1, rtol=rtol*1e-1)
 
-    N = sol_N.N[1:-1]
-    t = sol_N.t(N)
+        N = sol_N.N[1:-1]
+        t = sol_N.t(N)
 
-    # Solve in t
-    equations = Eqs_t(K, V)
-    ic = IC_t(N_0, phi_0)
-    events = Inflation(equations, direction=-1, terminal=True)
-    sol_t = solve(equations, ic, events=events, t_eval=t, atol=atol, rtol=rtol)
+        # Solve in t
+        equations = Eqs_t(K, V)
+        ic = IC_t(N_0, phi_0)
+        events = Inflation(equations, direction=-1, terminal=True)
+        sol_t = solve(equations, ic, events=events, t_eval=t, atol=atol*1e-1, rtol=rtol*1e-1)
 
-    # Check t-N consistency
-    atol *= 10
-    rtol *=10
-    assert_allclose(sol_t.N(t), N, atol, rtol)
-    assert_allclose(t, sol_N.t(N), atol, rtol)
+        # Check t-N consistency
+        assert_allclose(sol_t.N(t), N, atol, rtol)
+        assert_allclose(t, sol_N.t(N), atol, rtol)
 
-    # Check phi
-    assert_allclose(sol_t.phi(t), sol_N.phi(N), atol, rtol)
+        # Check phi
+        assert_allclose(sol_t.phi(t), sol_N.phi(N), atol, rtol)
 
-    # Check H
-    assert_allclose(sol_N.H(N), sol_t.H(t), atol, rtol)
+        # Check H
+        assert_allclose(sol_N.H(N), sol_t.H(t), atol, rtol)
